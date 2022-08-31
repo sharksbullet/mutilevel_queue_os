3f5fbdad7d02af596871eb646d09e2e1887e2d7f
sharksbullet <57445240+sharksbullet@users.noreply.github.com>
2022-09-01 00:42:27 +0700

    Update Cont.js

--- src/Cont.js
+++ src/Cont.js
@@ -4,11 +4,5 @@ import React, { createContext } from "react";
 const Cont = createContext();
 
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            rows:[]
-        };
-    }
+
 }
